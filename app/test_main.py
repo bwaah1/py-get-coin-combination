4d43013ce57db4df64b7e0f76b47de35cb29a0d0
bwaah1 <chykivan@gmail.com>
2024-04-04 22:19:02 +0300

    Fix flake8 error

--- app/test_main.py
+++ app/test_main.py
@@ -7,9 +7,11 @@ import pytest
     [
         pytest.param(1, [1, 0, 0, 0], id="1 cent: 1 penny"),
         pytest.param(15, [0, 1, 1, 0], id="15 cents: 1 nickel, 1 dime"),
-        pytest.param(17, [2, 1, 1, 0], id="17 cents: 2 dimes, 1 nickel, 1 penny"),
+        pytest.param(17, [2, 1, 1, 0], id="17 cents: 2 dimes, "
+                                          "1 nickel, 1 penny"),
         pytest.param(50, [0, 0, 0, 2], id="50 cents: 2 quarters"),
-        pytest.param(99, [4, 0, 2, 3], id="99 cents: 3 quarters, 2 dimes, 4 pennies")
+        pytest.param(99, [4, 0, 2, 3], id="99 cents: 3 quarters, "
+                                          "2 dimes, 4 pennies")
     ]
 )
 def test_return_different_coin_types(cents: int,
